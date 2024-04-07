42b86875aeae85a716014c616dbfa9fd5918908c
ruanaaf <Anna.G.NOSOVA@raiffeisen.ru>
2024-04-08 02:07:06 +0300

    wip

--- src/main/java/leo/landau/Main.java
+++ src/main/java/leo/landau/Main.java
@@ -9,21 +9,15 @@ import io.micronaut.runtime.Micronaut;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try {
-            // Загрузка свойств из файла application.properties
             Properties properties = new Properties();
             properties.load(Main.class.getResourceAsStream("/application.properties"));
 
-            // Загрузка содержимого PDF-файла
             byte[] originalPdfContent = Files.readAllBytes(Paths.get("src/main/resources/doc.pdf"));
 
-            // Создание печатей на PDF-страницах согласно переданным свойствам
             StampWriter.createStamps(originalPdfContent, properties);
-
-            System.out.println("Штампы успешно добавлены в PDF-файл.");
         } catch (IOException e) {
-            System.err.println("Произошла ошибка при чтении файлов или свойств.");
             e.printStackTrace();
         }
 
